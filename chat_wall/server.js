6949c2e8a13a43bfa0db6f4dd5e50b0cf63a7a23
aravindan swaminathan <arvind@aravindans-MacBook-Pro.local>
2017-05-08 23:50:40 -0700

    express and POST support

--- chat_wall/server.js
+++ chat_wall/server.js
@@ -1,26 +1,38 @@
 /* server code */
 
-const http = require('http')
-const fs = require('fs')
-const port = 8080
-const htmlFileName = './chat_wall.html'
+//const http = require('http');
+const fs = require('fs');
+const port = 8080;
+const htmlFileName = './chat_wall.html';
+const express = require('express');
+const bodyParser = require('body-parser');
+const chatHandler = express();
+
+chatHandler.use(bodyParser.urlencoded({ extended: true}));
 
 fs.readFile (htmlFileName, function(err, html) {
   if (err) {
     console.log(`file read error ${err}`)
   }
 
-  http.createServer (function (request, response) {
-    console.log(request.url)
-    response.writeHeader(200, {"Content-Type": "text/html"})
-    response.write(html)
-    response.end()
-  }).listen(port, (err) => {
-    if (err) {
-      return console.log('Not Good', err)
-    }
-    console.log(`server is listening on port ${port}`)
+  chatHandler.get('/', function (request, response) {
+    console.log(request.url);
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(html);
+    response.end();
   })
 })
 
+chatHandler.post('/send_message', function(request, response) {
+  console.log("got post");
+  console.log(request.body.message);
+  response.send('You sent the msg ' + request.body.message);
+})
+
+chatHandler.listen(port, function(err) {
+  if (err) {
+    return console.log('Not Good', err)
+  }
+  console.log(`server is listening on port ${port}`)
+})
 
