09edb6f176cdc04b06d77af460975d56f8352110
Aravindan Swaminathan <A.Swaminathan@247-inc.com>
2017-05-09 20:06:58 -0700

    handlebar

--- chat_wall/server.js
+++ chat_wall/server.js
@@ -1,32 +1,37 @@
 /* server code */
 
-//const http = require('http');
 const fs = require('fs');
 const port = 8080;
 const htmlFileName = './chat_wall.html';
+const chatMessagesFile = './chat_messages.txt';
 const express = require('express');
 const bodyParser = require('body-parser');
 const chatHandler = express();
+var html = fs.readFileSync(htmlFileName); // blocking read
 
 chatHandler.use(bodyParser.urlencoded({ extended: true}));
 
-fs.readFile (htmlFileName, function(err, html) {
-  if (err) {
-    console.log(`file read error ${err}`)
-  }
-
-  chatHandler.get('/', function (request, response) {
-    console.log(request.url);
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write(html);
-    response.end();
-  })
+chatHandler.get('/', function (request, response) {
+  console.log(request.url);
+  response.writeHeader(200, {"Content-Type": "text/html"});
+  response.write(html);
+  response.end();
 })
 
 chatHandler.post('/send_message', function(request, response) {
   console.log("got post");
   console.log(request.body.message);
-  response.send('You sent the msg ' + request.body.message);
+
+  // write to file
+  fs.appendFile(chatMessagesFile, request.body.message + "\n", function(err) {
+    if (err) {
+      return console.log(`file writing failed ${err}`);
+    }
+    console.log("wrote msg to file");
+  })
+  response.writeHeader(200, {"Content-Type": "text/html"});
+  response.write(html);
+  response.end();
 })
 
 chatHandler.listen(port, function(err) {
