441a9d6fa6f43bc01c97b1fef8a300e138ed0868
Aravindan Swaminathan <A.Swaminathan@247-inc.com>
2017-05-12 16:04:48 -0700

    send messages back

--- chat_wall/server.js
+++ chat_wall/server.js
@@ -7,7 +7,9 @@ const chatMessagesFile = './chat_messages.txt';
 const express = require('express');
 const bodyParser = require('body-parser');
 const chatHandler = express();
-var html = fs.readFileSync(htmlFileName); // blocking read
+var html = fs.readFileSync(htmlFileName, "utf8"); // blocking read
+//TODO: initialize the html page with old messages
+//var messages = fs.readFileSync(chatMessagesFile, "utf8"); // blocking read
 
 chatHandler.use(bodyParser.urlencoded({ extended: true}));
 
@@ -19,18 +21,26 @@ chatHandler.get('/', function (request, response) {
 })
 
 chatHandler.post('/send_message', function(request, response) {
+  var message = request.body.message;
+  message.replace(/([^\r])\n/g, "$1\r\n");
   console.log("got post");
-  console.log(request.body.message);
+  console.log(message);
 
   // write to file
-  fs.appendFile(chatMessagesFile, request.body.message + "\n", function(err) {
+  fs.appendFile(chatMessagesFile, request.body.message + "\r\n", function(err) {
     if (err) {
       return console.log(`file writing failed ${err}`);
     }
     console.log("wrote msg to file");
+
+    //TODO: send this message to client
   })
-  response.writeHeader(200, {"Content-Type": "text/html"});
-  response.write(html);
+
+  var messages = fs.readFileSync(chatMessagesFile, "utf8"); // blocking read
+  messages.replace(/([^\r])\n/g, "$1\r\n");
+  console.log(`sending POST data ${messages}`);
+  response.writeHeader(200, {"Content-Type": "text/plain"});
+  response.write(messages);
   response.end();
 })
 
